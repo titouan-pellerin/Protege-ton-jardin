c26f595cb8fd97555ea200b38d910c6b96e2cda1
Admin <titouan.pellerin@gmail.com>
2019-03-23 17:32:54 +0100

    Fenêtre "ajouter une plante"

---
+++ app/src/main/java/fr/visufo/titouan/jardin/CustomDialogClass.java
@@ -0,0 +1,46 @@
+package fr.visufo.titouan.jardin;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class CustomDialogClass extends Dialog implements
+        android.view.View.OnClickListener {
+
+    public Activity c;
+    public Dialog d;
+    public Button yes, no;
+
+    public CustomDialogClass(Activity a) {
+        super(a);
+        // TODO Auto-generated constructor stub
+        this.c = a;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.addplante_dialog);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        /*switch (v.getId()) {
+            case R.id.btn_yes:
+                c.finish();
+                break;
+            case R.id.btn_no:
+                dismiss();
+                break;
+            default:
+                break;
+        }
+        dismiss();*/
+    }
+}
--- app/src/main/java/fr/visufo/titouan/jardin/MainActivity.java
+++ app/src/main/java/fr/visufo/titouan/jardin/MainActivity.java
@@ -1,50 +1,76 @@
 package fr.visufo.titouan.jardin;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.content.res.AppCompatResources;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.leinardi.android.speeddial.SpeedDialActionItem;
+import com.leinardi.android.speeddial.SpeedDialView;
 
 public class MainActivity extends AppCompatActivity {
 
+    SpeedDialView mSpeedDialView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+      /* FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
+        });*/
+       addFab();
+        mSpeedDialView.setOnActionSelectedListener(new SpeedDialView.OnActionSelectedListener() {
+            @Override
+            public boolean onActionSelected(SpeedDialActionItem actionItem) {
+                switch (actionItem.getId()) {
+                    case R.id.fab_add_plant:
+                        CustomDialogClass cdd = new CustomDialogClass(MainActivity.this);
+                        cdd.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                        cdd.show();
+                        mSpeedDialView.close(); // To close the Speed Dial with animation
+                        return true; // false will close it without animation
+                    case R.id.fab_settings:
+                        mSpeedDialView.close(); // To close the Speed Dial with animation
+                        return true; // false will close it without animation
+                    default:
+                        break;
+                }
+                return true; // To keep the Speed Dial open
+            }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
+    public void addFab(){
+        mSpeedDialView = findViewById(R.id.speedDial);
+        mSpeedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.fab_add_plant, R.drawable.ic_plants)
+                .setFabBackgroundColor(ResourcesCompat.getColor(getResources(),R.color.colorAccent, getTheme()))
+              //  .setFabImageTintColor(ResourcesCompat.getColor(getResources(), R.color.white, getTheme()))
+                .setLabel("Ajouter une plante")
+                .create());
+
+        mSpeedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.fab_settings, R.drawable.ic_settings)
+                .setFabBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.colorAccent, getTheme()))
+                .setLabel("Paramètres")
+                .create());
+
     }
 }
